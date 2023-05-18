187aa3281579bf8ec599075d5cbb94564b43db41
knastasya <[km.nastya1@yandex.ru]>
2023-05-18 18:17:23 +0300

    created page of articles

---
+++ modules/articles.js
@@ -0,0 +1,14 @@
+const sectionFood = document.querySelector(".articles-topics-food");
+const sectionSport = document.querySelector(".articles-topics-sport");
+const sectionPsycho = document.querySelector(".articles-topics-psycho");
+const linkFood = document.querySelector("link-food");
+const linkSport = document.querySelector("link-sport");
+const linkPsycho = document.querySelector("link-psycho");
+
+sectionSport.style.display = "none";
+sectionPsycho.style.display = "grid";
+sectionFood.style.display = "none";
+
+// linkSport.addEventListener("click", function () {
+//   sectionFood.style.display = "none";
+// });
