28c7a6d92ddeee248fb3795e3001b9f60a286e20
AllaOz <ozcanalla@gmail.com>
2023-05-22 23:08:44 +0300

    test

--- modules/newproduct.js
+++ modules/newproduct.js
@@ -1,20 +1,20 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const addPopUpNewProduct = document.getElementById("productnotfound__popup-submit");
-    const closePopUpNewProduct = document.getElementById("productnotfound__popup-cancel");
-    const popUpNewProduct = document.getElementById("productnotfound__popup");
-    const popUpAddNewProduct = document.getElementById("newproduct__popup");
-  
-    function addNewProduct() {
-      event.preventDefault();
-      popUpNewProduct.style.display = "none";
-      popUpAddNewProduct.style.display = "block";
-    }
-  
-    function cancelNewProduct() {
-      event.preventDefault();
-      popUpNewProduct.style.display = "none";
-    }
-  
-    addPopUpNewProduct.addEventListener("click", addNewProduct);
-    closePopUpNewProduct.addEventListener("click", cancelNewProduct);
-  });
+const addPopUpNewProduct = document.getElementById("productnotfound__popup-submit");
+const closePopUpNewProduct = document.getElementById("productnotfound__popup-cancel");
+const popUpNotFound = document.getElementById("productnotfound__popup");
+const popUpAddNewProduct = document.getElementById("newproduct__popup");
+popUpAddNewProduct.style.display = "block";
+// popUpNotFound.style.display = "block";
+
+addPopUpNewProduct.addEventListener("click", addNewProduct);
+closePopUpNewProduct.addEventListener("click", cancelNewProduct);
+
+function addNewProduct(event) {
+    event.preventDefault();
+    popUpNotFound.style.display = "none";
+    popUpAddNewProduct.style.display = "block";
+}
+
+function cancelNewProduct(event) {
+    event.preventDefault();
+    popUpNotFound.style.display = "none";
+}
