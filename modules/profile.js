e55755f0ec1ff6042c4e64a0d80f0d0e9abb4099
Inna <melikhovainna@gmail.com>
2023-05-25 17:05:19 +0300

    resp profile finish

--- modules/profile.js
+++ modules/profile.js
@@ -2,20 +2,45 @@
 
 // chart
 const doughnut = document.getElementById('doughnut');
+const macro = document.querySelectorAll(`.profile-user__macro-amount_span`);
+const macroAmount = [];
+
+for(let macroElement of macro){
+    macroAmount.push(`${macroElement.textContent}`);
+}
+
 
 new Chart(doughnut, {
     type: 'doughnut',
     data: {
-        // labels: ['Выполнено за неделю', 'Не выполнено за неделю'],
+        // labels: ['Углеводы', 'Белки', 'Жиры'],
         datasets: [{
-            // label: 'Задач',
-            data: [5, 6, 8],
+            label: 'грамм',
+            data: macroAmount,
             borderWidth: 1,
-            backgroundColor: ['#E16527','#089BAA', '#FCDC29'],
+            backgroundColor: ['#089BAA','#E16527', '#FCDC29'],
             borderColor: ['rgba(0, 0, 0, 0.7)','rgba(0, 0, 0, 0.7)', 'rgba(0, 0, 0, 0.7)']
         }]
     }
 });
 // chart
+
+
+// data
+let kkalTaken = 0;
+const kkalTakenNode = document.querySelector(`.profile-user__kkal-amount`);
+
+for (let i=0; i<macroAmount.length; i++){
+    i===2? kkalTaken += macroAmount[i]*9: kkalTaken += macroAmount[i]*4;
+}
+
+kkalTakenNode.textContent = `${Math.round(kkalTaken)}`;
+// data
+
+// time
+const dateNode = document.querySelector(`.profile-user__date-value`);
+dateNode.textContent = moment().format("DD/MM/YYYY")
+
+// time
 
 // ИННА
