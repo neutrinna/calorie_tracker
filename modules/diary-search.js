7405bbc9b670980bf28c748cf944708dbe8c7cfd
AllaOz <ozcanalla@gmail.com>
2023-05-26 23:05:21 +0300

    test api

--- modules/diary-search.js
+++ modules/diary-search.js
@@ -3,40 +3,78 @@ const popUpAddNewProduct = document.querySelector(".diary__newproduct__popup");
 popUpAddNewProduct.style.display = "none";
 const commentElem = document.createElement('div');
 
-
-
 document.addEventListener("DOMContentLoaded", function() {
   const forms = document.querySelectorAll(".diary__form__search");
 
   forms.forEach((form, index) => {
-    const searchResultDiv = document.getElementById(`search__show__result-${index + 1}`);
-    const input = form.querySelector(`#diary-search-${index + 1}`);
+    const input = document.getElementById(`diary-search-${index + 1}`);
 
     input.addEventListener("keypress", function(event) {
       if (event.key === "Enter") {
         event.preventDefault();
 
+        const searchResultDiv = document.getElementById(`search__show__result-${index + 1}`);
         const searchResult = input.value;
         commentElem.classList.add('elem');
         commentElem.innerHTML = `
           <div class="search-result">${searchResult}</div>`;
 
         const grammInput = document.createElement('input');
+        grammInput.classList.add('inputStyle')
         grammInput.setAttribute('type', 'number');
         grammInput.setAttribute('placeholder', 'грамм');
-        grammInput.style.backgroundColor = 'white';
-        grammInput.style.border = '1px solid rgb(10, 155, 171)';
-        grammInput.style.width = '100px';
-        grammInput.style.height = '40px';
-        grammInput.style.borderRadius = '10px';
-        grammInput.style.marginLeft = '25%';
-        grammInput.style.textAlign = 'center';
 
         const checkbox = document.createElement('input');
         checkbox.setAttribute('type', 'checkbox');
         checkbox.style.marginRight = '25%';
         checkbox.style.verticalAlign = 'middle';
         checkbox.style.transform = 'scale(1.5)';
+
+        checkbox.addEventListener('change', function() {
+          if (checkbox.checked) {
+            fetch(`https://world.openfoodfacts.org/api/v0/product/${searchResult}`)
+              .then(response => response.json())
+              .then(res => {
+                console.log(res);
+                if (res.status === 1) {
+                  const product = res.product;
+                  const productName = product.product_name;
+                  const calories = product.nutriments['energy-kcal'];
+                  const carbs = product.nutriments.carbohydrates;
+                  const proteins = product.nutriments.proteins;
+                  const fats = product.nutriments.fat;
+
+                  const accordeonDivs = document.querySelector(`.accordeon${index + 1}.accordeon_hidden`)
+                  const diaryTableStringDiv = document.createElement('div');
+                  diaryTableStringDiv.classList.add('diary-table-string');
+
+                  const mealNameDiv = document.createElement('div');
+                  mealNameDiv.classList.add('diary-table-string__meal-name');
+                  mealNameDiv.innerHTML = `
+                    <p>${productName}</p>`;
+
+                  const columnNamesDiv = document.createElement('div');
+                  columnNamesDiv.classList.add('diary-table-string__column-names');
+                  columnNamesDiv.style.display = 'flex';
+                  columnNamesDiv.innerHTML = `
+                    <div>${grammInput.value}</div>
+                    <div>${calories * grammInput.value / 100}</div>
+                    <div>${carbs * grammInput.value / 100}</div>
+                    <div>${proteins * grammInput.value / 100}</div>
+                    <div>${fats * grammInput.value / 100}</div>`;
+
+                  searchResultDiv.innerHTML = '';
+                  accordeonDivs.appendChild(diaryTableStringDiv);
+                  diaryTableStringDiv.appendChild(mealNameDiv);
+                  diaryTableStringDiv.appendChild(columnNamesDiv);
+                 
+                }
+              })
+              .catch(error => {
+                console.log('Error:', error);
+              });
+          }
+        });
 
         fetch(`https://world.openfoodfacts.org/api/v0/product/${searchResult}`)
           .then(response => response.json())
@@ -51,8 +89,8 @@ document.addEventListener("DOMContentLoaded", function() {
               commentElem.appendChild(grammInput);
               commentElem.appendChild(checkbox);
               searchResultDiv.innerHTML = '';
-              searchResultDiv.appendChild(commentElem);
               input.value = '';
+              searchResultDiv.appendChild(commentElem);
             } else {
               openPopupProductNotFound();
             }
@@ -77,7 +115,6 @@ openPopUpNewProduct.addEventListener("click", openNewProduct);
 closePopUpNotFound.addEventListener("click", closeNotFound);
 closePopUpNewProduct.addEventListener("click", closeNewProduct);
 
-
 function openNewProduct(event) {
   event.preventDefault();
   popUpNotFound.style.display = "none";
@@ -87,7 +124,6 @@ function openNewProduct(event) {
 function closeNotFound(event) {
   event.preventDefault();
   popUpNotFound.style.display = "none";
-  document.getElementById('diary-search').value = '';
 }
 
 function closeNewProduct(event) {
