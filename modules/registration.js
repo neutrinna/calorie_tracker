84629e0b9d5ca906f6dfd273f91e8de528932955
Inna Melikhova <melikhovainna@gmail.com>
2023-07-26 13:22:00 +0300

    Revert "input validation"

--- modules/registration.js
+++ modules/registration.js
@@ -1,3 +1,4 @@
+  // получаем элементы формы
   const nameInput = document.getElementById("registration__text_name");
   const surnameInput = document.getElementById("registration__text_surname");
   const bdayInput = document.getElementById("registration__text_bday");
@@ -6,57 +7,41 @@
   const passwordRepeatInput = document.getElementById("registration__text_passwordRepeat");
   const submitBtn = document.getElementById("registration__submit");
   
-  function validateDate(input) {
-    let enteredDate = new Date(input.value);
-    const minDate = new Date(input.min);
-    const maxDate = new Date(input.max);
-    if (enteredDate < minDate || enteredDate > maxDate) {
-      alert("Неверная дата рождения!");
-      input.value = ""; 
-    }
-  }
-
-  function validateEmail(input) {
-    let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(input.value)) {
-      alert("Неправильный формат электронной почты!");
-      input.value = ""; 
-    }
-  }
-
-  function validatePassword(input) { 
-    const password = passwordInput.value; 
-        
-    if (password.length < 6) {
-      alert("Пароль должен содержать не менее 6 символов и не более 12");
-      input.value = ""; 
-    }
-  } 
 
   function registrationAdd() { 
-    const name = nameInput.value[0].toUpperCase() + nameInput.value.slice(1).toLowerCase();
-    const surname = surnameInput.value[0].toUpperCase() + surnameInput.value.slice(1).toLowerCase();  
+    const name = nameInput.value[0].toUpperCase() + nameInput.value.slice(1); 
+    const surname = surnameInput.value[0].toUpperCase() + surnameInput.value.slice(1); 
     const bday = bdayInput.value; 
     const email = emailInput.value; 
     const password = passwordInput.value; 
     const passwordRepeat = passwordRepeatInput.value; 
   
     function checkPassword() { 
+      // if (password === '' || passwordRepeat === '') { 
+      //   alert('Заполните оба поля'); 
+      //   return false; 
+      // } 
+  
       if (password !== passwordRepeat) { 
         return false; 
       } 
+  
+      // Пароль прошел проверку 
       return true; 
     } 
   
     if (name && surname && bday && email && password && passwordRepeat && checkPassword()) { 
+      // получаем текущий массив пользователей (если его нет, то создаем пустой массив) 
       let userData = JSON.parse(localStorage.getItem("userData")) || []; 
   
+      // проверка, что такой пользователь уже не зарегистрирован (по email) 
       const existingUser = userData.find((user) => user.email === email); 
       if (existingUser) { 
         alert("Пользователь с таким email уже зарегистрирован!"); 
         return; 
       } 
   
+     // создаем объект с данными нового пользователя 
       const newUser = { 
         name, 
         surname, 
@@ -65,9 +50,11 @@
         password, 
       }; 
   
+      // добавляем нового пользователя в массив и сохраняем его в localStorage 
       userData.push(newUser); 
       localStorage.setItem("userData", JSON.stringify(userData)); 
   
+      // выводим сообщение об успешной регистрации 
       alert(`Пользователь ${name} ${surname} успешно зарегистрирован!`); 
   
       const userSessions = JSON.parse(localStorage.getItem("userSessions")) || {}; 
@@ -75,6 +62,7 @@
       console.log(userSessions); 
       localStorage.setItem("userSessions", JSON.stringify(userSessions)); 
   
+      // сбрасываем значения полей ввода 
       nameInput.value = ""; 
       surnameInput.value = ""; 
       bdayInput.value = ""; 
@@ -87,7 +75,13 @@
     } 
   }
   
+
+  // // добавляем обработчик на кнопку "Зарегистрироваться"
+  // submitBtn.addEventListener("click", registrationAdd);
+  
+  // функции открытия/закрытия попапа "Регистрация"
   function openRegistration() {
+    // e.eventPreventDefault();
     document.getElementById("registration__pop-up").style.display = "block";
     popUpHide()
   }
@@ -96,6 +90,7 @@
     document.getElementById("registration__pop-up").style.display = "none";
   }
   
+  // функции открытия/закрытия попапа "Настройки"
   function openCustomization() {
     document.getElementById("myDropdown").style.display = "none"
     document.getElementById("customization__pop-up").style.display = "block";
@@ -121,6 +116,7 @@
     document.getElementById("customization__pop-up").style.display = "none";
   }
   
+  // выносим получение текущего массива пользователей в отдельную функцию
   function getUsers() {
     return JSON.parse(localStorage.getItem("userData")) || [];
   }
